918b6af5cd0ebc3fe1204510047d3b6070f54eba
Jubiko31 <jubiko.macaberidze1@gmail.com>
2023-03-07 15:14:57 +0400

    bootstrap config; header & footer components

--- frontend/src/app/App.js
+++ frontend/src/app/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import { Container } from 'react-bootstrap'
+import { Header, Footer } from '../components'
 
 function App() {
   return (
-    <div className="App">
-      Hello from React
+    <div>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <h1>Welcome</h1>
+        </Container>
+      </main>
+        <Footer />
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './app/App';
+import './styles/bootstrap.min.css';
+import './styles/index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
