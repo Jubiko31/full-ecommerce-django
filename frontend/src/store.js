51393d5af66b9f4a622c7cc178b2de36807221bf
Jubiko31 <jubiko.macaberidze1@gmail.com>
2023-06-16 15:15:09 +0400

    added product reducers, actions, constants

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,8 +1,30 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit';
 import thunk from 'redux-thunk';
+import {
+    productListReducer,
+    productDetailsReducer,
+    productDeleteReducer,
+    productCreateReducer,
+    productUpdateReducer,
+    productReviewCreateReducer,
+    productTopRatedReducer,
+} from './reducers/productReducers'
 
-const reducer = combineReducers({});
-const initialState = {};
+const userInfoFromStorage = localStorage.getItem('userInfo') ?
+    JSON.parse(localStorage.getItem('userInfo')) : null
+
+const reducer = combineReducers({
+    productList: productListReducer,
+    productDetails: productDetailsReducer,
+    productDelete: productDeleteReducer,
+    productCreate: productCreateReducer,
+    productUpdate: productUpdateReducer,
+    productReviewCreate: productReviewCreateReducer,
+    productTopRated: productTopRatedReducer,
+});
+const initialState = {
+    userLogin: { userInfo: userInfoFromStorage },
+};
 const middleware = [thunk];
 
 const store = configureStore({
